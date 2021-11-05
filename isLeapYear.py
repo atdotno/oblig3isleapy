1de5c25cec4f775ea8139cfe839f7620980c78c3
atdotno <93791534+atdotno@users.noreply.github.com>
2021-11-05 19:55:02 +0100

    Create isLeapYear.py

---
+++ isLeapYear.py
@@ -0,0 +1,16 @@
+
+def isLeapYear(year):
+    '''returns if a year is a leap year'''
+    print("is", year, "a leapyear?")
+    return year % 4 == 0 and year % 100 != 0 or year % 400 == 0
+
+
+
+print(isLeapYear(1922))
+
+
+print(isLeapYear(2004))
+
+print(isLeapYear(2009))
+
+print(isLeapYear(2000))
